b3740d4ea118651e0786595d49432a6c325e0b1e
Amoghmotagi5 <motagiamogh5@gmail.com>
2026-09-10 00:26:19 +0530

    Update max_of_four.c

--- max_of_four.c
+++ max_of_four.c
@@ -19,4 +19,45 @@ int main(){
   else{
     printf("max of %d,%d,%d,%d is %d\n",a,b,c,d,d);
   }
+
+   //#automatic method
+  printf("Automatic method\n\n");
+
+  int n1,n2,n3,n4,max;
+  printf("Enter four no\n");
+  scanf("%d%d%d%d",&n1,&n2,&n3,&n4);
+
+    if(n1>=n2)
+        max=n1;
+    else
+        max=n2;
+    if(max<=c)
+        max=c;
+    else
+        max=max;
+    if(max<=d)
+        max=d;
+    else
+        max=max;
+    printf("Max of %d,%d,%d,%d is %d\n\n",n1,n2,n3,n4,max);
+
+    //#fully automatic
+    printf("Fully Automatic\n\n");
+    int arr[4],temp;
+    printf("Enter four number\n");
+    for(int i=0;i<4;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    for(int i=0;i<4;i++){
+        for(int j=i+1;j<4;j++){
+            if(arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    printf("max of %d,%d,%d,%d is %d\n",arr[0],arr[1],arr[2],arr[3],arr[3]);
+   return 0;
 }
